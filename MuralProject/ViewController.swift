1261e1eb3d58ad84efd132b55bbd46ea85097ced
Johan Flores <jflores3123@stu.d214.org>
2022-04-26 12:19:12 -0500

    Initial Commit

---
+++ MuralProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MuralProject
+//
+//  Created by Johan Flores  on 06/02/1401 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
